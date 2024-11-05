065dbc108024366a72637ff3dad51deeb3a04f28
Duda <duda@mobradio.com.br>
2024-11-05 13:04:37 -0300

    add  new component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <header className="banner">
+      <img src="/imgs/banner.png" alt="Banner da tela principal" />
+      ;\
+    </header>
+  );
+};
+export default Banner;
